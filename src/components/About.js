579350b3a61ba21c060c1047e6d4a696fd27716a
Hoai Thuong <thuongnvhde170266@fpt.edu.vn>
2024-11-01 15:50:31 +0700

    initial code

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="container my-5">
+      <h2>About Us</h2>
+      <p>Information about the company and its mission.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-4">
+      <p>&copy; 2023 Kaira. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
